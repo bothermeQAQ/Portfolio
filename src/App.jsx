fb85d1fe774f3096e98d749bb7cbda5680069bfd
ZiyaoZzz <z1093935026@gmail.com>
2024-12-22 11:34:00 -0500

    交互与字体更新

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,18 @@ function App() {
         <h1>Yonghao Wang's Portfolio</h1>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Me</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Me</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </nav>
       </header>
